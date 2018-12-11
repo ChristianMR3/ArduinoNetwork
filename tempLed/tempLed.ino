08b79c2637452d9f3f0e5cdf72f422a64bb61949
ChristianMR3 <43175659+ChristianMR3@users.noreply.github.com>
2018-12-11 09:48:12 -0600

    Update tempLed.ino

--- tempLed/tempLed.ino
+++ tempLed/tempLed.ino
@@ -1,68 +1,68 @@
+/************************ configuracion de io.adafruit *******************************/
 
-/************************ Adafruit IO Configuration *******************************/
-
-// visit io.adafruit.com if you need to create an account,
+/************************ configuracion de adafruit *******************************/
+//para esto necesitamos una cuenta de adafruit
+// visita io.adafruit.com si necesitas crear una cuenta,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME  "PabloBarroso"
-#define IO_KEY       "2bd2f2b95c0e49b5b4f0789bdf18e993"
 
-/******************************* WIFI Configuration **************************************/
+#define IO_USERNAME  "PabloBarroso"  ///introduimos nuestro nombre de ususario
+#define IO_KEY       "***************" //la llave de nuestro usuario (por custiones de seguridad no se puede poner la clave)
+                                       //ya que cada ususario tiene una diferente ademas se vincula a tu e-mail
+//el nombre de ususario y la llave te las proporcionan una vez creas tu cuenta
+
+/******************************* Configuration del WIFI **************************************/
 //
-#define WIFI_SSID       "INFINITUMycq8"
-#define WIFI_PASS       "B3rn4rd0N0H4C3"
+#define WIFI_SSID       "INFINITUMycq8" // nombre de la red a la cual la placa estara conectada
+#define WIFI_PASS       "B3rn4rd0N0H4C3" // contraseña de la red a la cual la placa estara conectada
 
 #include "AdafruitIO_WiFi.h"
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-/************************ Main Program Starts Here *******************************/
-#include <ESP8266WiFi.h>
-#include <AdafruitIO.h>
+/************************ Main Programa Inicia aqui *******************************/
+#include <ESP8266WiFi.h> //libreria de la placa ESP8266
+#include <AdafruitIO.h> //libreria para poder conectarnos con adafruit
 #include <Adafruit_MQTT.h>
-#include <ArduinoHttpClient.h>
-#include <DHT11.h>
-#define LED_PIN 0
+#include <ArduinoHttpClient.h> //cliente de http
+#include <DHT11.h> // libreria del sensor de temperatura
+#define LED_PIN 0  // led
 
 // button state
 int current = 0;
 int last = 0;
 
-// set up the 'command' feed
+// especificamos el 'command' feed "el feed es el nombre del proyecto que creamos en adafruit"
 AdafruitIO_Feed *command = io.feed("test1");
 
 
-float temp, hum;
+float temp, hum; //declaramos las varables y el pin para el DHT11
 int sensor = 2;
 DHT11 dht11(sensor);
-
-String readString;
-String m;
-
 
 void setup() {
 
   pinMode(LED_PIN, OUTPUT);
 
-  // start the serial connection
+  // iniciamos la conexion serial
   Serial.begin(115200);
 
 
-  // connect to io.adafruit.com
+  // conectamos con io.adafruit.com
   Serial.print("Connecting to Adafruit IO");
   io.connect();
 
   // set up a message handler for the 'command' feed.
-  // the handleMessage function (defined below)
+  // la funcion handleMessage (defininida mas abajo)
   // will be called whenever a message is
   // received from adafruit io.
   command->onMessage(handleMessage);
 
-  // wait for a connection
+  // esperamos la conexion con adafruit
   while (io.status() < AIO_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
 
-  // we are connected
+  //se imprime este mensaje si nos concectamos correctamente
   Serial.println();
   Serial.println(io.statusText());
 
@@ -70,54 +70,48 @@ void setup() {
 
 void loop() {
 
-  // io.run(); is required for all sketches.
-  // it should always be present at the top of your loop
-  // function. it keeps the client connected to
-  // io.adafruit.com, and processes any incoming data.
+  // io.run();es requerido en todos los sketches.
+  // simepre debe estar presente en la parte superior de el la funcion loop()
+  //mantiene el cliente conectado con i0.adafruit.com, y procesa cualquier dato de entrada
   io.run();
 
 
 }
-
-// this function is called whenever a 'command' message
-// is received from Adafruit IO. it was attached to
-// the command feed in the setup() function above.
+//esta funcion es llamada siempre que un mensaje 'command'
+// es recivido desde Adafruit IO. que fue definida
+// en el command feed en la funcion setup().
 void handleMessage(AdafruitIO_Data *data) {
 
   int command = data->toInt();
 
-  dht11.read(hum, temp);
+  dht11.read(hum, temp); // leemos los datos del sensor tanto la temperatura como la humedad
 
-
-  while (Serial.available()) {
-    delay(50);
-    char c = Serial.read();
-    readString += c;
-  }
-
-
-  if (command == 0) { //light up the LED
+// se resive la instruccion por el asistente de google y nos retorna un 0
+  if (command == 0) { // apaga el LED
     Serial.print("received <- ");
     Serial.println(command);
     digitalWrite(LED_PIN, LOW);
   }
-  if (command == 1) {
+  // se resive la instruccion por el asistente de google y nos retorna un 1
+  if (command == 1) { /// enciende el led
     Serial.print("received <- ");
     Serial.println(command);
     digitalWrite(LED_PIN, HIGH);
   }
-  if (command == 2) {
+  // se resive la instruccion por el asistente de google y nos retorna un 2
+  if (command == 2) { // nos muestra la temperatura en pantalla
     Serial.print("received <- ");
     Serial.println(command);
     Serial.println("Temperatura");
     Serial.println(temp);
   }
-  if(command ==3) {
+  // se resive la instruccion por el asistente de google y nos retorna un 3
+  if(command ==3) { // nos muestra la humadad en pantalla
     Serial.print("received <- ");
     Serial.println(command);
     Serial.println("Humedad");
     Serial.println(hum);
-    delay(1000);
+   // delay(1000);
   }
   
 }
